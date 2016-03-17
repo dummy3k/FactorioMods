31fd29893951232ccea7d510f219aa0cf7ff638f
dummy3k <l4711@gmx.net>
2016-03-17 21:51:25 +0100

    small fixes

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(name='FactorioMods',
       description='Read factoriomods.com/recently-updated and update git repo',
       author='Dummy3k',
       author_email='l4711@gmx.net',
-      # url='https://github.com/dummy3k/to_be_done',
+      url='https://github.com/dummy3k/FactorioMods',
       packages=['FactorioMods'],
       scripts=['scripts/fm_update.py'],
 	  install_requires=[
           'colorlog', 
-		  'coloredlogs', 
       ],
      )
